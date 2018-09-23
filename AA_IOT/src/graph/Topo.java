1535f8e19da18d8479cc9efd1feb8cef20793dd4
marco <s105062638@m105.nthu.edu.tw>
2018-09-23 20:19:44 +0800

    Functions deleted

--- AA_IOT/src/graph/Topo.java
+++ AA_IOT/src/graph/Topo.java
@@ -8,8 +8,6 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.*;
 
 public class Topo {
-	private static final Random random = new Random();
-
 	private Topo() {
 	}
 
@@ -36,36 +34,6 @@ public class Topo {
 		}
 		return consumed;
 	} // end method getEnergyConsumed
-
-	public static double getMinAggregationEnergyConsumed(Vertex bs, List<Vertex> mecs, Vertex cloud, double size,
-			DijkstraShortestPath<Vertex, DefaultEdge> d) {
-		double min = Double.MAX_VALUE;
-		double energyConsumed;
-		// calculate for each MEC
-		for (Vertex mec : mecs) {
-			energyConsumed = getAggregationEnergyConsumed(bs, mec, cloud, size, d);
-			if (energyConsumed <= min) {
-				min = energyConsumed;
-			}
-		}
-		return min;
-	} // end method getMinAggregationEnergyConsumed
-
-	/*
-	 * calculates the energy consumption of aggregation path from the serving BS
-	 * to the cloud server D by computing the energy consumption of paths from
-	 * BS to the MEC server v and from v to D
-	 */
-	private static double getAggregationEnergyConsumed(Vertex bs, Vertex mec, Vertex cloud, double size,
-			DijkstraShortestPath<Vertex, DefaultEdge> d) {
-		double consumed = 0;
-		if (d == null)
-			return -1;
-		consumed += (Topo.getEnergyConsumed(bs, mec, size, d) - Vertex.MEC_FORWARDING_ENERGY + Vertex.MEC_ENERGY);
-		// flow size becomes size * (1 - Vertex.RATIO) after traversing the MEC
-		consumed += Topo.getEnergyConsumed(mec, cloud, size * (1 - Vertex.RATIO), d);
-		return consumed;
-	} // end method getAggregationEnergyConsumed
 
 	public static GraphImporter<Vertex, DefaultEdge> createImporter() {
 		// create vertex provider
