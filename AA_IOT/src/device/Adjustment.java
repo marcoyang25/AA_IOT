616397d0b65dd257b6c8cde59bd15c462f10aba4
marco <s105062638@m105.nthu.edu.tw>
2018-09-24 16:45:13 +0800

    Added some methods

--- AA_IOT/src/device/Adjustment.java
+++ AA_IOT/src/device/Adjustment.java
@@ -21,25 +21,41 @@ public class Adjustment {
 		Queue<Device> Q = new LinkedList<>();
 		Set<Device> unadjustedDevices = new HashSet<>(selectedDevices);
 
+		// for each location and for each device in its selected group members
+		// set, add the location to the device LocationsResponsibleFor
+		for (Location location : locations.values()) {
+			for (Device device : location.getSelectedGroup().getMembers()) {
+				device.addLocationResponsibleFor(location);
+			}
+		}
+
 		// calculate processing MEC for each location
 		for (Location location : locations.values()) {
 			setMinCommnicationEnergyMEC(location, mecs, f);
 		}
 
+		// running n times
 		for (int n = 0; n < 1; n++) {
 			while (!unadjustedDevices.isEmpty()) {
+				Device maxEnergyDevice = findMaxEnergyDevice(unadjustedDevices, f);
+				Q.offer(maxEnergyDevice);
+				while (!Q.isEmpty()) {
+					Device deviceToAdjust = Q.poll();
+					unadjustedDevices.remove(deviceToAdjust);
+					// adjust this device
 
+				} // end while
 			} // end while
 		} // end for
 
 	} // end method adjust
 
-	public static void setMinCommnicationEnergyMEC(Location location, List<Vertex> mecs,
+	private static void setMinCommnicationEnergyMEC(Location location, List<Vertex> mecs,
 			FloydWarshallShortestPaths<Vertex, DefaultEdge> f) {
 		Vertex MinCommnicationEnergyMEC = null;
 		Double minEnergy = Double.POSITIVE_INFINITY;
 		for (Vertex mec : mecs) {
-			Double CommnicationEnergy = LocationCommnicationEnergy(location, mec, f);
+			Double CommnicationEnergy = locationCommnicationEnergy(location, mec, f);
 			if (CommnicationEnergy <= minEnergy) {
 				minEnergy = CommnicationEnergy;
 				MinCommnicationEnergyMEC = mec;
@@ -47,15 +63,38 @@ public class Adjustment {
 		}
 		location.setCommunicationEnergy(minEnergy);
 		location.setProcessingMEC(MinCommnicationEnergyMEC);
-	}
+	} // end method setMinCommnicationEnergyMEC
 
-	public static double LocationCommnicationEnergy(Location location, Vertex mec,
+	private static double locationCommnicationEnergy(Location location, Vertex mec,
 			FloydWarshallShortestPaths<Vertex, DefaultEdge> f) {
 		double consumed = 0;
 		for (Device device : location.getSelectedGroup().getMembers()) {
 			consumed += Topo.getEnergyConsumed(device.getAssociatedMEC(), mec, f);
 		}
 		return consumed;
-	} // end method LocationCommnicationEnergy
+	} // end method locationCommnicationEnergy
+
+	private static double deviceCommunicationEnergy(Device device, FloydWarshallShortestPaths<Vertex, DefaultEdge> f) {
+		double consumed = 0;
+		for (Location location : device.getLocationsResponsibleFor()) {
+			consumed += Topo.getEnergyConsumed(device.getAssociatedMEC(), location.getProcessingMEC(), f);
+		}
+		return consumed;
+	} // end method deviceCommunicationEnergy
+
+	private static Device findMaxEnergyDevice(Set<Device> unadjustedDevices,
+			FloydWarshallShortestPaths<Vertex, DefaultEdge> f) {
+		Device MaxEnergyDevice = null;
+		double max = Double.NEGATIVE_INFINITY;
+		for (Device device : unadjustedDevices) {
+			double connectionEnergy = device.getConnectionEnergy().get(device.getAssociatedMEC());
+			double communicationEngergy = deviceCommunicationEnergy(device, f);
+			if ((connectionEnergy + communicationEngergy) <= max) {
+				max = connectionEnergy + communicationEngergy;
+				MaxEnergyDevice = device;
+			}
+		}
+		return MaxEnergyDevice;
+	} // end method findMaxEnergyDevice
 
 }
